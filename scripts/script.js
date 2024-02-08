f15f97ffe4fc113bced56dcfa2b95eac66feaa39
jaydengreen23 <157416940+jaydengreen23@users.noreply.github.com>
2024-02-08 00:46:46 -0600

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
   
   console.log("Top: " +  $("img").position().top + " Left: " + $("img").position().left);
-  
-  
+
+  // #containment-wrapper { width: 95%; height:150px; border:2px solid #ccc; padding: 10px; }
+  // $( "#draggable3" ).draggable({ containment: "#containment-wrapper", scroll: false })
+  // $( "#draggable" ).draggable({ axis: "y" });
   $("img").draggable({
     stop: function(){
       console.log('stopping?');
@@ -20,13 +22,22 @@ $(document).ready(function(){
       }
     }
   });
-  });
   
-//   $("img").draggable(function(){
-//     
-//   }
-//   });
-  
-  
-  
-  //TODO if position on far right tell user they won
+  function myFunction(element, color){
+    element.style.color = color;
+  }
+
+  function displayText() {
+        // Get the text container element
+        var textContainer = document.getElementById('text-container');
+        
+        // Create a new text node
+        var newText = document.createTextNode('Your AWESOME');
+        
+        // Clear any existing text in the container
+        textContainer.innerHTML = '';
+        
+        // Append the new text node to the container
+        textContainer.appendChild(newText);
+    }
+});
